ffcf05e71896450f8207580fd9cc8946d5ef4617
soft.coder <soft.coder@mail.ru>
2026-03-26 19:46:09 +0400

    feat(shared-util): export id generator
    Refs: #3

--- projects/shared-util/src/public-api.ts
+++ projects/shared-util/src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of shared-util
  */
 
+export { IdGenerator } from './lib/id-generator';
 export { LocalStorage } from './lib/local-storage';
 export type { getContext } from './lib/text-conext';
