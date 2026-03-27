ed01d7cc9287763e367adcbfdd20b42cdaf10f1d
soft.coder <soft.coder@mail.ru>
2026-03-27 22:27:03 +0400

    fix(feature-article): add actions to article table
    Refs: #14

--- projects/feature-article/src/lib/article-table/article-table.ts
+++ projects/feature-article/src/lib/article-table/article-table.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ArticlePreviewModel } from '@pta/model';
-import { Table } from '@pta/ui';
+import { Button, Table } from '@pta/ui';
 
 @Component({
   selector: 'pta-article-table',
-  imports: [Table],
+  imports: [Table, Button, RouterLink],
   templateUrl: 'article-table.html',
   styleUrl: 'article-table.scss'
 })
