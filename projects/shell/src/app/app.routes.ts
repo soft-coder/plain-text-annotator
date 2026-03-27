8be5e6c36fa032eba7be0c7f7b0b123397d6eaa2
soft.coder <soft.coder@mail.ru>
2026-03-27 18:27:23 +0400

    feat: add routing for article list
    Refs: #10

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'article',
+    loadChildren: () => import('@pta/feature-article').then(m => m.routes)
+  },
+  {
+    path: '**', redirectTo: 'article', pathMatch: 'full'
+  }
+];
