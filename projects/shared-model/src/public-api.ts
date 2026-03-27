a1af1ef3b825a1b96ae856c601c777701902d557
soft.coder <soft.coder@mail.ru>
2026-03-27 11:44:00 +0400

    feat: add global css variables
    add css variables to shell project
    fix export ArticlePreview
    
    Refs: #8

--- projects/shared-model/src/public-api.ts
+++ projects/shared-model/src/public-api.ts
@@ -4,4 +4,4 @@
 
 export type { AnnotationModel } from './lib/model/annotation'
 export type { ArticleModel } from './lib/model/article';
-export type { ArticlePreview } from './lib/model/article';
+export type { ArticlePreviewModel } from './lib/model/article-preview';
